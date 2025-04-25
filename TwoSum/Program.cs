6ba3e331f09aed9ad2ebec2ab11bdc5471aa0e15
Darko <darkopokoracki@gmail.com>
2025-04-25 09:56:49 +0200

    Solution for the TwoNums

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -10,6 +10,36 @@ namespace TwoSum
     {
         static void Main(string[] args)
         {
+            int[] nums = { 7, 11, 2, 15 };
+            int target = 9;
+
+            int[] result = TwoSum(nums, target);
+
+            Console.WriteLine("Result: ");
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine($"Indices {i + 1}: {result[i]}");
+            }
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] indices = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        indices[0] = i;
+                        indices[1] = j;
+                        break;
+                    }
+                }
+            }
+
+            return indices;
         }
     }
 }
