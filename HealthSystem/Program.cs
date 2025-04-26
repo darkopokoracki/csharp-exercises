258a9e09dd0ffba6b6b3ac6d11bc8633a3ba9e3b
Darko <darkopokoracki@gmail.com>
2025-04-26 08:43:47 +0200

    Longest Common prefix - solution

---
+++ HealthSystem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystem
+{
+    internal class Program
+    {
+
+        public interface IHealth
+        {
+            string GetPersonalData();
+        }
+            
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- LongestCommonPrefix/Program.cs
+++ LongestCommonPrefix/Program.cs
@@ -10,6 +10,30 @@ namespace LongestCommonPrefix
     {
         static void Main(string[] args)
         {
+            string[] strs = { "flower", "flow", "flight" };
+
+            string longestPrefix = LongestCommonPrefix(strs);
+            Console.WriteLine($"Longest prefix: {longestPrefix}");
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0)
+                return "";
+
+            string prefix = strs[0];
+
+            for (int i = 1; i < strs.Length; i++)
+            {
+                while (!strs[i].StartsWith(prefix))
+                {
+                    if (prefix.Length == 0)
+                        return "";
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                }
+            }
+
+            return prefix;
         }
     }
 }
