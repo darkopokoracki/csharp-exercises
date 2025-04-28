0d4024686fa4e19167fa4f350132e29102477f3e
Darko <darkopokoracki@gmail.com>
2025-04-28 08:50:30 +0200

    Remove Element solution

--- RemoveElement/Program.cs
+++ RemoveElement/Program.cs
@@ -9,5 +9,29 @@ namespace RemoveElement
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 2, 2, 3 };
+            int val = 3;
+
+            int notEqualToVal = RemoveElement(nums, val);
+            Console.WriteLine($"Not Equal to val: {notEqualToVal}");
+        }
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+
+            return k;
+        }
     }
 }
