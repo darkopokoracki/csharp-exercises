10a8f1c600b3467b4d9c7d064a8bee6f25b2534f
Darko <darkopokoracki@gmail.com>
2025-04-29 09:05:44 +0200

    Length of Last Word

---
+++ LengthOfLastWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LengthOfLastWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "       Fly me   to   the moon     ";
+
+            Console.WriteLine($"Length od Last Word is: {LengthOfLastWord(s)}");
+        }
+
+        public static int LengthOfLastWord(string s)
+        {
+            string[] splitedString = s.Trim().Split(' ');
+            // string lastWord = splitedString[splitedString.Length - 1];
+            string lastWord = splitedString.Last();
+            return lastWord.Length;
+        }
+    }
+}
