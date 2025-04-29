e04ba1c0ae9cf53765d03c8cd9b7593a45f8ee54
Darko <darkopokoracki@gmail.com>
2025-04-29 08:47:18 +0200

    Index of the first occurence

---
+++ IndexOfTheFirstOccurence/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexOfTheFirstOccurence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string haystack = "sadbutsad";
+            string needle = "sad";
+
+            Console.WriteLine($"Index of the first occuracne is: {StrStr(haystack, needle)}");
+        }
+
+        public static int StrStr(string haystack, string needle)
+        {
+            return haystack.IndexOf(needle);
+        }
+    }
+}
