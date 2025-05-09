437f53f084be74b4c16101877499ac2eba851c79
Darko <darkopokoracki@gmail.com>
2025-05-10 00:29:08 +0200

    Valid Palindrome solution algo

--- ValidPalindrome/Program.cs
+++ ValidPalindrome/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ValidPalindrome
@@ -10,6 +11,27 @@ namespace ValidPalindrome
     {
         static void Main(string[] args)
         {
+            string sentence = "A man, a plan, a canal: Panama";
+            Console.WriteLine($"Is valid palindrome: {IsPalindrome(sentence)}");
+        }
+
+        public static bool IsPalindrome(string s)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            Regex empt = new Regex(@"\s+");
+            string str = rgx.Replace(s, "");
+            string withoutSpace = empt.Replace(str, "");
+            string finalString = withoutSpace.ToLower();
+
+            for (int i = 0; i < finalString.Length; i++)
+            {
+                if (finalString[i] != finalString[finalString.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
